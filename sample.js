92ea21b5fc4f64c1bde8386d59e4f5924b04bc91
Takashi <Takashi@gmail.com>
2019-08-10 18:10:57 +0900

    入力した生徒を追加する機能を実装した

--- sample.js
+++ sample.js
@@ -3,10 +3,10 @@ new Vue({
   el: '#app',
   // dataは、定義されたVue.jsのインスタンスが持つ属性（値）
   data: {
-    name: 'A山B郎',
+    name: '竹山たかし',
     cource: 'Webエンジニアコース',
-    acceptance_period: '2019年01月期',
-    default_last_id: 3,
+    acceptance_period: '2019年07月期',
+    default_last_id: 4,
     students: [
       { id: 1, name: '野呂浩良', cource: '機械学習コース', acceptance_period: '2019年01月期' },
       { id: 2, name: '富永修司', cource: 'Webエンジニアコース', acceptance_period: '2017年11月期' },
@@ -18,7 +18,11 @@ new Vue({
     addStudent: function() {
         // この function() { } の中にaddStudentメソッド
         // （入力欄に入力された値と、ID値（最後に登録されたID値+1の値）が新たに出現する機能）の処理を書く
-        
+        this.students.push({
+          id: this.default_last_id ++,
+          name: this.name,
+          cource: this.cource,
+          acceptance_period: this.acceptance_period
       })
     }
   }
